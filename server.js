62d3b271ed5942196dea5b111a2b092658ef6161
Arturs Cavniss <arthur.mailz@gmail.com>
2023-11-08 21:38:13 +0000

    fix front-end

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import morgan from 'morgan';
 import * as dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
-import cloudinary from 'cloudinary'
+import cloudinary from 'cloudinary';
 dotenv.config();
 //custom imports
 //router
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 
 app.use(cookieParser());
 app.use(express.json());
@@ -47,9 +47,9 @@ app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
-app.get('*', (req, res)=> {
-  res.sendFile(path.resolve(__dirname, './public/index.html'))
-})
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
+});
 
 app.use('*', (req, res) => {
   res.status(404).json({ msg: 'page is not found' });
